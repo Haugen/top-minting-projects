638ede84dc5477ec8c8e0b5640eee2f765fec6f2
Haugen <hej@tobiashaugen.se>
2022-10-11 15:31:33 +0200

    some first play to view the data live

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/future/image";
 import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
@@ -23,17 +24,39 @@ const Home = () => {
       return result?.data?.data as Project[];
     },
     {
-      staleTime: 2000,
+      staleTime: 3000,
+      refetchInterval: 3000,
     }
   );
 
+  if (!data) return null;
+
   return (
-    <div>
+    <div className="m-10">
+      <h1 className="text-8xl uppercase">Top Minters</h1>
+      <p className="my-10 text-2xl">
+        Top minting projects on the Ethereum blockchain in the last five
+        minutes.
+      </p>
       {data?.map((project, i) => {
         return (
-          <div key={i} className="flex">
-            <div>{project.name}</div>
-            <div>{project.mint_count}</div>
+          <div key={i} className="flex mb-10">
+            <div className="w-[150px] h-[150px] mr-10">
+              {project.image_url && (
+                <Image
+                  className="rounded-full"
+                  src={project.image_url}
+                  width={150}
+                  height={150}
+                  alt=""
+                />
+              )}
+            </div>
+            <div>
+              <h2 className="text-2xl">{project.name}</h2>
+              <div className="text-5xl">{project.mint_count}</div>
+              <div className="text-sm">{project.standard}</div>
+            </div>
           </div>
         );
       })}
